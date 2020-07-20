575c0a10a1a7a6b5dbaf72313e0dfb9b2bf715d9
Rob Little <rlittletht@users.noreply.github.com>
2020-07-19 22:42:55 -0700

    nuget publish spec

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TCore.Settings")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Read and write settings, registry persisted")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TCore.Settings")]
